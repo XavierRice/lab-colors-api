ee877b4ba703d300597f059c4af0159e1ef3642e
xavierrice <xavier.rice@gmail.com>
2023-11-02 12:06:43 -0400

    working on deployment

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -6,6 +6,7 @@ const cn = {                              // Our connection object
     port: process.env.PG_PORT,
     database: process.env.PG_DATABASE,
     user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD
   };
   
  //connects the color_dev db 
